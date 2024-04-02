18b3d659cf5ea9c7d4f3069df1bb3cfcf22eb623
Yoojian <soiseaubleu@gmail.com>
2024-04-02 16:00:46 +0900

    commit

--- polls/urls.py
+++ polls/urls.py
@@ -6,11 +6,19 @@ app_name = 'polls'
 
 urlpatterns = [
     # ex: /polls/
-    path("", views.index, name="index"),
+    path("", views.IndexView.as_view(), name="index"),
     # ex: /polls/1/
-    path("<int:question_id>/", views.detail, name="detail"),
+    path("<int:question_id>/", views.DetailView.as_view(), name="detail"),
     # ex: /polls/2/results/
-    path("<int:question_id>/results/", views.results, name="results"),
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
     # ex: /polls/3/vote/
     path("<int:question_id>/vote/", views.vote, name="vote"),
+    # CreateView
+    path('question/new/', views.QuestionCreateView.as_view(), name='question_new'),
+    path('question/<int:pk>/choice/new/', views.ChoiceCreateView.as_view(), name='choice_new'),
+    # UpdateView
+    path('question/<int:pk>/update/', views.QuestionUpdateView.as_view(), name='question_update'),
+    path('choice/<int:pk>/update/', views.ChoiceUpdateView.as_view(), name='choice_update'),
+    # DeleteView
+    path('question/<int:pk>/delete/', views.QuestionDeleteView.as_view(), name='question_delete'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,24 +1,120 @@
-from django.http import HttpResponse
+from django.db.models.base import Model as Model
+from django.db.models.query import QuerySet
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import Question, Choice
 from django.template import loader
+from django.views import generic
+from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse, reverse_lazy
+from django.db.models import F, Sum
 
-# 뷰 추가하기
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+# DeleteView
+class QuestionDeleteView(generic.DeleteView):
+    model = Question
+    template_name = 'polls/question_delete.html'
+    success_url = reverse_lazy('polls:index')
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question})
 
-# def vote(request, question_id):
-#     # choice 데이터에서 해당하는 값에 votes를 1 더하기
-#     c = Choice.objects.get(pk=8)
-#     c.votes += 1
-#     c.save()
-#     return HttpResponse("You're voting on question %s." % question_id)
+
+# ChoiceUpdateView
+# 연습문제1.설문조사 앱에서 선택지 업데이트 기능 구현
+class ChoiceUpdateView(generic.UpdateView):
+    model = Choice
+    fields = ['choice_text']
+    template_name = 'polls/choice_update_form.html' 
+    success_url = reverse_lazy('polls:index')
+
+# UpdateView
+class QuestionUpdateView(generic.UpdateView):
+    model = Question
+    fields = ['question_text', 'pub_date']
+    template_name = 'polls/question_update_form.html' 
+    success_url = reverse_lazy('polls:index') 
+
+
+
+# ChoiceCreateView
+# 연습문제1.설문조사 앱에 새로운 선택지 추가 기능 구현
+class ChoiceCreateView(generic.CreateView):
+    model = Choice
+    fields = ['choice_text']
+    template_name = 'polls/choice_form.html'
+    def form_valid(self, form):
+        form.instance.question = get_object_or_404(Question, pk=self.kwargs['pk'])
+        return super().form_valid(form)
+    def get_success_url(self):
+        return reverse('polls:detail', kwargs={'question_id': self.kwargs['pk']})
+
+# CreateView
+class QuestionCreateView(generic.CreateView):
+    model = Question
+    fields = ['question_text']
+    template_name = 'polls/question_form.html'
+    success_url = reverse_lazy('polls:index')
+
+
+
+# IndexView
+class IndexView(generic.ListView):
+    # 페이지 연결
+    # [app_name]/[model_name]_list.html
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    # 화면에 표시될 객체
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by("-pub_date")[:5]
+    
+    # # 연습문제1. 최근 설문조사 질문 목록
+    # def get_queryset(self):
+    #     return Question.objects.order_by("-pub_date")[:3]
+    
+    # # 연습문제2. 가장 많은 투표를 받은 질문
+    # # q -> q.choice_set.all() : cs -> sum[c.votes for c in cs]
+    # # [sum([c.votes for c in q.choice_set.all()] for in Question.objects.all()]
+    # # sorted(qs. key=lambda q : sum([c.votes for c in q.choice_set.all()]), reverse=True)
+    # def get_queryset(self):
+    #     return Question.objects.annotate(total=Sum('choice__votes')).order_by('-total')
+
+    # # 연습문제3. 아직 투표가 없는 질문 목록
+    # def get_queryset(self):
+    #     return Question.objects.annotate(total=Sum('choice__votes')).filter(total=0)
+    
+
+# DetailView
+# # 연습문제1
+# class DetailView(generic.DetailView):
+#     model = Question
+#     template_name = "polls/detail.html"
+
+#     def get_object(self):
+#         q_id = self.kwargs['question_id']
+#         question = get_object_or_404(Question, pk=q_id)
+#         return question
+    
+# 연습문제2
+# polls/question_detail.html 생성
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/question_detail.html"
+
+    def get_object(self):
+        q_id = self.kwargs['question_id']
+        question = get_object_or_404(Question, pk=q_id)
+        return question
+
+
+
+# ResultsView
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
+
+
 
 # 단축키 index 뷰 업데이트와 같은 내용
-from django.shortcuts import render
 def index(request):
     # 최근 추가한 5개 보임
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
@@ -27,17 +123,15 @@ def index(request):
     return render(request, "polls/index.html", context)
 
 
+
 # 객체가 존재하지 않을 때 get() 을 사용하여 Http404 예외를 발생
-from django.shortcuts import get_object_or_404, render
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, "polls/detail.html", {"question": question})
 
 
+
 # vote의 detail 템플릿을 수정 -> form 요소 포함
-from django.http import HttpResponseRedirect
-from django.urls import reverse
-from django.db.models import F
 def vote(request, question_id):
 
     # # choice 데이터에서 해당하는 값에 votes를 1 더하기
@@ -70,3 +164,17 @@ def vote(request, question_id):
 
 
 
+# # 뷰 추가하기
+# def detail(request, question_id):
+#     return HttpResponse("You're looking at question %s." % question_id)
+
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, "polls/results.html", {"question": question})
+
+# def vote(request, question_id):
+#     # choice 데이터에서 해당하는 값에 votes를 1 더하기
+#     c = Choice.objects.get(pk=8)
+#     c.votes += 1
+#     c.save()
+#     return HttpResponse("You're voting on question %s." % question_id)
